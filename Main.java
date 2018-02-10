bfe636c44aae7a2dd03e6e8471cf794f88784b80
val17102 <32184574+val17102@users.noreply.github.com>
2018-02-09 20:44:14 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Random;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -21,33 +22,29 @@ public class Main {
     
     static Sorts s= new Sorts();
      public void Main(){
-         Generador g = new Generador();
       
      }
      
       public static void main(String[] args){
-      
-        String linea = "";
-        Integer arr[] = new Integer[3000];
-        try{
-           int i = 0;
-           File arc = new File("generado.txt");
-           BufferedReader br = new BufferedReader(new FileReader(arc));
-           while((linea = br.readLine()) != null){
-               linea = br.readLine();
-               Integer x = Integer.parseInt(linea);
-               arr[i] = x;
-	       System.out.println(linea);
-	       i++;
-           }
+         Random ran = new Random();
+         Integer[] x = new Integer[3000];
+         for (int i= 0; i < x.length; i++)
+             {
+                x[i] = ran.nextInt(101);
+              }
 	//for(int j = 0; j < arr.length ; j++)
 	//System.out.println(arr[j]);
         // s.mergeSort(arr,0,arr.length-1);
-        }catch(FileNotFoundException ex){
-            System.out.println("Error no encontrado");
-        }catch(IOException ex){
-           System.out.println("Error");
-        }
+        System.out.println("Gnome Sort");
+        System.out.println(Arrays.toString(s.gnomeSort(x)));
+        System.out.println("Insertion Sort");
+        System.out.println(Arrays.toString(s.insertionSort(x, x.length)));
+        System.out.println("Quick Sort");
+        System.out.println(Arrays.toString(s.quickSort(x, 0, x.length-1)));
+        System.out.println("Merge Sort");
+        System.out.println(Arrays.toString(s.mergeSort(x, 0, x.length-1)));
+        System.out.println("Radix Sort");
+        System.out.println(Arrays.toString(s.radixSort(x)));
         //ar = gnomeSort(arr);
         //mergeSort(arr, 0 , arr.length-1);
         //s.radixSort(a);
